d4ade41d5901c076bae515dd7c60157dc7d531a7
WAS-D <67601886+WAS-D@users.noreply.github.com>
2020-10-02 12:21:53 -0400

    Update and rename Tick-Tack-Toe.cpp to Cpp.cpp

---
+++ Cpp.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //includes the console in and out functions (for console apps
+#include <stdio.h> //includes the fopen and printf functions (mostly for debugging)
+#include <string> //includes the string functionality
+int main() {
+    std::cout << /*Console out*/;
+    std::getline (std::cin,/*a string*/)
+}
