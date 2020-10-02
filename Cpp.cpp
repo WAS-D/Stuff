8ccd373f52efe359eacd0ae42a12d64dd9a7f737
WAS-D <67601886+WAS-D@users.noreply.github.com>
2020-10-02 13:00:01 -0400

    Update Cpp.cpp

--- Cpp.cpp
+++ Cpp.cpp
@@ -1,7 +1,10 @@
-#include <iostream> //includes the console in and out functions (for console apps
-#include <stdio.h> //includes the fopen and printf functions (mostly for debugging)
-#include <string> //includes the string functionality
+/***********************************************
+* A commented C++ template file for reference. *
+***********************************************/
+#include <iostream> //Includes the console in and out functions (for console apps).
+#include <stdio.h> //Includes the fopen and printf functions (mostly for debugging).
+#include <string> //Includes the string functionality.
 int main() {
     std::cout << /*Console out*/;
-    std::getline (std::cin,/*a string*/)
+    std::getline (std::cin,/*a string*/) //Exports input to a string.
 }
